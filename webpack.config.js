a1e51796378c8f371be4e3e7c4d2f5da6f5b2350
simon <simon.roberge@vimaec.com>
2021-10-01 14:49:44 -0400

    webpack typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,27 @@ const path = require('path');
 
 const config = {
     mode: 'development',
-    entry: './viewer.js',
+    entry: './viewer.ts',
     output: {
         filename: "vim-webgl-viewer.js",
         path: path.resolve(__dirname, 'public'),
         library: 'vim',
     },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devtool: 'source-map',  // generate source map
 };
 
-module.exports = () => {return config;}
+module.exports = () => {
+    return config;
+}
