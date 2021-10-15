539a18c1bbb1df3ced86ef281a4d2ff83421eac0
simon <simon.roberge@vimaec.com>
2021-10-15 11:20:45 -0400

    publish

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
-set "origin=%cd%"
-cd %1
-del /F /Q *.*
-robocopy "%origin%/dist" %1 *.*
+REM set "origin=%cd%"
+REM cd %1
+REM del /F /Q *.*
+robocopy "./dist" %1 *.* /MIR
 git add -A
 git commit -m "publish"
 git push
