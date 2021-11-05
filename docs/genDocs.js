dacb03a46df3f0ca94dc097d60b1a1df78986294
simon <simon.roberge@vimaec.com>
2021-11-05 14:32:17 -0400

    updated genDocs and mustache to write style url

--- docs/genDocs.js
+++ docs/genDocs.js
@@ -2,24 +2,30 @@
     Updates the index.html and index-dev.html files.
     Only needs to be run when the index.mustache.html file is updated.
 */
-const fs = require('fs');
-const mustache = require('mustache');
+const fs = require('fs')
+const mustache = require('mustache')
 
-function genDoc(templateFile, outputFile, inputData)
-{
-    const template = fs.readFileSync(templateFile, "utf-8");
-    const output = mustache.render(template, inputData);
-    fs.writeFileSync(outputFile, output);    
+function genDoc (templateFile, outputFile, inputData) {
+  const template = fs.readFileSync(templateFile, 'utf-8')
+  const output = mustache.render(template, inputData)
+  fs.writeFileSync(outputFile, output)
 }
 
-const templateFile = 'docs/index.mustache.html';
-const prodFile = 'docs/index.html';
-const devFile = 'docs/index-dev.html'; 
-const distFile = 'dist/index.html'; 
-const prodData = { viewerUrl: "https://unpkg.com/vim-webgl-viewer@latest" };
-const devData = { viewerUrl: "https://unpkg.com/vim-webgl-viewer@dev" };
-const distData = { viewerUrl: "vim-webgl-viewer.iife.js" };
+const templateFile = 'docs/index.mustache.html'
+const prodFile = 'docs/index.html'
+const devFile = 'docs/index-dev.html'
+const distFile = 'dist/index.html'
+const unpckg = 'https://unpkg.com/vim-webgl-viewer'
+const prodData = {
+  viewerUrl: unpckg + '@latest',
+  style: unpckg + '@latest/dist/style.css'
+}
+const devData = {
+  viewerUrl: unpckg + '@dev',
+  style: unpckg + '@dev/dist/style.css'
+}
+const distData = { viewerUrl: 'vim-webgl-viewer.iife.js', style: 'style.css' }
 
-genDoc(templateFile, prodFile, prodData);
-genDoc(templateFile, devFile, devData);
-genDoc(templateFile, distFile, distData);
+genDoc(templateFile, prodFile, prodData)
+genDoc(templateFile, devFile, devData)
+genDoc(templateFile, distFile, distData)
