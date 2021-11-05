d7715dfa2157b39b6a0aabc883e527ceb5fa90d1
simon <simon.roberge@vimaec.com>
2021-11-04 20:09:31 -0400

    testing css release

--- src/ViewerDocument.tsx
+++ src/ViewerDocument.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import urlLogo from './assets/logo.png'
 import { ViewerState } from './viewer'
-import './styles.css'
+import './style.css'
 
 export class ViewerDocument {
   canvas: HTMLCanvasElement
